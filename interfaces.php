1b88546de214d54a6ab2971cdfe1314197664171
key <a000058556@gmail.com>
2022-12-01 17:21:16 +0800

    move

--- interfaces.php
+++ interfaces.php
@@ -1839,13 +1839,13 @@ include("head.inc");
         print_info_box($savemsg);
       }
 ?>
-        <!-- 上方DHCP Server(v4/v6)選單 -->
-        <ul class="nav nav-tabs" data-tabs="tabs" id="maintabs">
-            <li class="active"><a data-toggle="tab" id="interfaces_tab" url="/interfaces.php?if=<?=$if; ?>"><?=gettext("Interfaces(".$if.")"); ?></a></li>
-            <li><a data-toggle="tab" id="dhcpv4_tab" url="/services_dhcp.php?if=<?=$if; ?>"><?=gettext("DHCP Server(v4)"); ?></a></li>
-            <li><a data-toggle="tab" id="dhcpv6_tab" url="/services_dhcpv6.php?if=<?=$if; ?>"><?=gettext("DHCP Server(v6)"); ?></a></li>
-        </ul>
         <section class="col-xs-12">
+          <!-- 上方DHCP Server(v4/v6)選單 -->
+          <ul class="nav nav-tabs" data-tabs="tabs" id="maintabs">
+              <li class="active"><a data-toggle="tab" id="interfaces_tab" url="/interfaces.php?if=<?=$if; ?>"><?=gettext("Interfaces(".$if.")"); ?></a></li>
+              <li><a data-toggle="tab" id="dhcpv4_tab" url="/services_dhcp.php?if=<?=$if; ?>"><?=gettext("DHCP Server(v4)"); ?></a></li>
+              <li><a data-toggle="tab" id="dhcpv6_tab" url="/services_dhcpv6.php?if=<?=$if; ?>"><?=gettext("DHCP Server(v6)"); ?></a></li>
+          </ul>
           <form method="post" name="iform" id="iform">
               <div class="tab-content content-box col-xs-12 __mb">
                 <div class="table-responsive">
