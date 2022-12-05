26ac2c9a2ffe463a7458967f3c7145f9603ba736
key <a000058556@gmail.com>
2022-12-05 15:14:49 +0800

    re

--- interfaces.php
+++ interfaces.php
@@ -1821,31 +1821,31 @@ include("head.inc");
       window_highlight_table_option();
   });
 
-  // tab功能控制
-  $( document ).ready(function() {
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-              console.log("-------------e-------------");
-              console.log(e);
-              // let group_by = e.target.href.split('#')[1];
-              // load_chart(group_by);
-              // $("#heading_label").text(e.target.text);
-          });
+  // // tab功能控制
+  // $( document ).ready(function() {
+  //   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  //             console.log("-------------e-------------");
+  //             console.log(e);
+  //             // let group_by = e.target.href.split('#')[1];
+  //             // load_chart(group_by);
+  //             // $("#heading_label").text(e.target.text);
+  //         });
 
-          let selected_tab = window.location.hash != "" ? window.location.hash : "#action";
-          console.log("-------------selected_tab-------------");
-          console.log(selected_tab);
-          $('a[href="' +selected_tab + '"]').tab('show');
-          $('.nav-tabs a').on('shown.bs.tab', function (e) {
-              history.pushState(null, null, e.target.hash);
-              console.log("---------e.target.hash-----------");
-              console.log(e.target.hash);
-          });
-          $(window).on('hashchange', function(e) {
-              console.log("---------window.location.hash-----------");
-              console.log(window.location.hash);
-              $('a[href="' + window.location.hash + '"]').click()
-          });
-  });
+  //         let selected_tab = window.location.hash != "" ? window.location.hash : "#action";
+  //         console.log("-------------selected_tab-------------");
+  //         console.log(selected_tab);
+  //         $('a[href="' +selected_tab + '"]').tab('show');
+  //         $('.nav-tabs a').on('shown.bs.tab', function (e) {
+  //             history.pushState(null, null, e.target.hash);
+  //             console.log("---------e.target.hash-----------");
+  //             console.log(e.target.hash);
+  //         });
+  //         $(window).on('hashchange', function(e) {
+  //             console.log("---------window.location.hash-----------");
+  //             console.log(window.location.hash);
+  //             $('a[href="' + window.location.hash + '"]').click()
+  //         });
+  // });
 </script>
 
 <!-- 頁面內容生成開始 -->
