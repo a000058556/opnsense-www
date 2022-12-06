c40dca898f62d0970470942d943cd8cc172cef40
key <a000058556@gmail.com>
2022-12-06 14:13:59 +0800

    $if_r = $if.'#relay'; header(url_safe('Location: /interfaces.php?if=%s', array($if_r)));

--- interfaces.php
+++ interfaces.php
@@ -645,7 +645,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
           
               echo('<br/>原始$pconfig資料內容<br/>');
               print_r ($ppconfig);
-              header(url_safe('Location: /interfaces.php?if=%s', ($if.'#relay')));
+              $if_r = $if.'#relay';
+              header(url_safe('Location: /interfaces.php?if=%s', array($if_r)));
           }
           exit;
       }
