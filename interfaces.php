8598985271a356913dd50ca45f8c3d8c63e1c06d
key <a000058556@gmail.com>
2022-12-06 15:58:57 +0800

    add relayv6 all

--- interfaces.php
+++ interfaces.php
@@ -596,6 +596,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     }
     $relay_pconfig['agentoption'] = isset($config['dhcrelay']['agentoption']);
 
+/***************************************************************************************************************
+ * relay use end / relayv6 use start
+ ***************************************************************************************************************/
+
+    $relayv6_pconfig['enable'] = isset($config['dhcrelay6']['enable']);
+    if (empty($config['dhcrelay6']['interface'])) {
+        $relayv6_pconfig['interface'] = array();
+    } else {
+        $relayv6_pconfig['interface'] = explode(",", $config['dhcrelay6']['interface']);
+    }
+    if (empty($config['dhcrelay6']['server'])) {
+        $relayv6_pconfig['server'] = "";
+    } else {
+        $relayv6_pconfig['server'] = $config['dhcrelay6']['server'];
+    }
+    $relayv6_pconfig['agentoption'] = isset($config['dhcrelay6']['agentoption']);
+
+/***************************************************************************************************************
+ * relay use end
+ ***************************************************************************************************************/
+
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // 當$_SERVER['REQUEST_METHOD'] === 'POST' / 表格送出資料
     // $_POST = 表格傳入參數與內容
@@ -638,16 +659,60 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
           if (!empty($ifgroup)) {
               header(url_safe('Location: /interfaces.php?if=%s&group=%s', array($if, $ifgroup)));
           } else {
+              // echo('<br/>$pconfig資料內容<br/>');
+              // print_r ($pconfig);
+              
+              // echo ('<br/>原始$relay_pconfig回傳值<br/>');
+              // print_r ($prelay_pconfig);
+          
+              // echo('<br/>原始$pconfig資料內容<br/>');
+              // print_r ($ppconfig);
+
+              header(url_safe("Location: /interfaces.php?if=%s", array($if)));
+          }
+          exit;
+      }
+    }elseif ($pconfig["relayv6_Submit"] == "Save") { 
+      $input_errors = array();
+      $pconfig = $_POST;
+      // $if $ifgroup跳轉頁面用
+      $ifgroup = !empty($_GET['group']) ? $_GET['group'] : '';
+      $if = $_GET['if'];
+
+      /* input validation */
+      $reqdfields = explode(" ", "server interface");
+      $reqdfieldsn = array(gettext("Destination Server"), gettext("Interface"));
+
+      do_input_validation($pconfig, $reqdfields, $reqdfieldsn, $input_errors);
+
+      if (!empty($pconfig['server'])) {
+          $checksrv = explode(",", $pconfig['server']);
+          foreach ($checksrv as $srv) {
+              if (!is_ipaddrv6($srv)) {
+                  $input_errors[] = gettext("A valid Destination Server IPv6 address must be specified.");
+              }
+          }
+      }
+
+      if (count($input_errors) == 0) {
+          $config['dhcrelay6']['enable'] = !empty($pconfig['enable']);
+          $config['dhcrelay6']['interface'] = implode(",", $pconfig['interface']);
+          $config['dhcrelay6']['agentoption'] = !empty($pconfig['agentoption']);
+          $config['dhcrelay6']['server'] = $pconfig['server'];
+          write_config();
+          plugins_configure('dhcrelay', false, array('inet6'));
+          if (!empty($ifgroup)) {
+              header(url_safe('Location: /interfaces.php?if=%s&group=%s', array($if, $ifgroup)));
+          } else {
               echo('<br/>$pconfig資料內容<br/>');
               print_r ($pconfig);
               
-              echo ('<br/>原始$relay_pconfig回傳值<br/>');
-              print_r ($prelay_pconfig);
+              echo ('<br/>原始$relayv6_pconfig回傳值<br/>');
+              print_r ($prelayv6_pconfig);
           
               echo('<br/>原始$pconfig資料內容<br/>');
               print_r ($ppconfig);
-              // $if_r = $if."\#relay";
-              header(url_safe("Location: /interfaces.php?if=%s", array($if)));
+              // header(url_safe("Location: /interfaces.php?if=%s", array($if)));
           }
           exit;
       }
@@ -3906,8 +3971,6 @@ include("head.inc");
  * relay start
  ***************************************************************************************************************/
 
-
-
 // 取interface
  $iflist = get_configured_interface_with_descr();
 
@@ -3924,10 +3987,7 @@ if (is_array($config['dhcpd'])) {
     }
 }
 $service_hook = 'dhcrelay';
-
 ?> 
-
-
 
           <div id="relay" class="tab-pane fade in">
           <div class="container-fluid">
@@ -4013,11 +4073,106 @@ $service_hook = 'dhcrelay';
           </div>
           <!-- relay  end -->
 
-          <!-- relayv6 start -->
-          <div id="relayv6" class="tab-pane fade in">
+<?php
+/***************************************************************************************************************
+ * relayv6 start
+ ***************************************************************************************************************/
+$dhcpdv6_enabled = false;
+if (is_array($config['dhcpdv6'])) {
+    foreach($config['dhcpdv6'] as $intf => $dhcp) {
+        if (isset($dhcp['enable']) && !empty($config['interfaces'][$intf]['enable'])) {
+            $dhcpdv6_enabled = true;
+        }
+    }
+}
 
+$service_hook = 'dhcrelay6';
+?>
+          
+          <div id="relayv6" class="tab-pane fade in">
+            <div class="container-fluid">
+              <div class="row">
+                <?php if ($dhcpdv6_enabled):
+                  print_info_box(gettext('The DHCPv6 server is currently enabled. Cannot enable the DHCPv6 relay while the DHCPv6 server is enabled on any interface.'));
+                else: ?>
+                <section class="col-xs-12">
+                  <div class="content-box">
+                    <form method="post" name="iform" id="iform">
+                      <div>
+                        <div class="table-responsive">
+                          <table class="table table-striped opnsense_standard_table_form">
+                            <tr>
+                              <td style="width:22%"><strong><?=gettext("DHCPv6 Relay configuration"); ?></strong></td>
+                              <td style="width:78%; text-align:right">
+                                <small><?=gettext("full help"); ?> </small>
+                                <i class="fa fa-toggle-off text-danger"  style="cursor: pointer;" id="show_all_help_page"></i>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td><i class="fa fa-info-circle text-muted"></i> <?= gettext('Enable') ?></td>
+                              <td>
+                                <input name="enable" type="checkbox" value="yes" <?=!empty($relayv6_pconfig['enable']) ? "checked=\"checked\"" : ""; ?>/>
+                                <strong><?=gettext("Enable DHCPv6 relay on interface");?></strong>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td><a id="help_for_interface" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?= gettext('Interface(s)') ?></td>
+                              <td>
+                                <select name="interface[]" multiple="multiple" class="selectpicker">
+        <?php
+                                $iflist = get_configured_interface_with_descr();
+                                foreach ($iflist as $ifent => $ifdesc):
+                                    if (!is_ipaddrv6(get_interface_ipv6($ifent))) {
+                                        continue;
+                                    }?>
+
+                                  <option value="<?=$ifent;?>" <?=!empty($relayv6_pconfig['interface']) && in_array($ifent, $relayv6_pconfig['interface']) ? " selected=\"selected\"" : "";?> >
+                                    <?=$ifdesc;?>
+                                  </option>
+        <?php
+                                endforeach;?>
+                                </select>
+                                <div class="hidden" data-for="help_for_interface">
+                                  <?=gettext("Interfaces without an IPv6 address will not be shown."); ?>
+                                </div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td><a id="help_for_agentoption" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Append circuit ID");?></td>
+                              <td>
+                                <input name="agentoption" type="checkbox" value="yes" <?=!empty($relayv6_pconfig['agentoption']) ? "checked=\"checked\"" : ""; ?> />
+                                <div class="hidden" data-for="help_for_agentoption">
+                                  <?= gettext('If this is checked, the DHCPv6 relay will append the circuit ID (interface number) and the agent ID to the DHCPv6 request.') ?>
+                                </div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td><a id="help_for_server" href="#" class="showhelp"><i class="fa fa-info-circle"></i></a> <?=gettext("Destination server");?></td>
+                              <td>
+                                <input name="server" type="text" value="<?=!empty($relayv6_pconfig['server']) ? htmlspecialchars($relayv6_pconfig['server']):"";?>" />
+                                <div class="hidden" data-for="help_for_server">
+                                  <?= gettext('This is the IPv6 address of the server to which DHCPv6 requests are relayed. You can enter multiple server IPv6 addresses, separated by commas.') ?>
+                                </div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td></td>
+                              <td>
+                                <input name="relayv6_Submit" type="submit" class="btn btn-primary" value="<?=html_safe(gettext('Save'));?>" />
+                              </td>
+                            </tr>
+                          </table>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </section>
+                <?php endif ?>
+              </div>
+            </div>
           </div>
           <!-- relayv6 end -->
+          
           </div>
           <!-- tab-content end -->
         </section>
